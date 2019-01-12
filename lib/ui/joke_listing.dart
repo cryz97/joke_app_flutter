29003f8e6fd3bf749dda239f1520f6bba01da108
Chris <christian.macias@cceo.com.mx>
2019-01-11 18:22:36 -0600

    orientation and tablet view

--- lib/ui/joke_listing.dart
+++ lib/ui/joke_listing.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:joke_app_orientation/model/joke.dart';
 
 
 class JokeListing extends StatelessWidget {
+
+  final ValueChanged<Joke> jokeSelectedCallback;
+  final Joke jokeSelected;
+
+  const JokeListing({@required this.jokeSelectedCallback, this.jokeSelected});
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView(
+      children: jokesList.map((joke){
+        return Container(
+          child: ListTile(
+            title: Text(joke.setup),
+            onTap:  () => jokeSelectedCallback(joke),
+            selected: jokeSelected == joke,
+          ),
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(5)
+          ),
+        );
+      }).toList(),
+    );
   }
 }
--- lib/ui/master_detail_screen.dart
+++ lib/ui/master_detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:joke_app_orientation/model/joke.dart';
+import 'package:joke_app_orientation/ui/joke_details.dart';
+import 'package:joke_app_orientation/ui/joke_listing.dart';
 
 class MasterDetailScreen extends StatefulWidget {
   @override
@@ -6,8 +9,61 @@ class MasterDetailScreen extends StatefulWidget {
 }
 
 class _MasterDetailScreenState extends State<MasterDetailScreen> {
+
+  static const int tabletBreakpoint = 600;
+  Joke _selectedJode;
+
+  Widget _buildMobileLayout() {
+    return JokeListing(jokeSelectedCallback: (jokeSelected) {
+        Navigator.push(context,
+              MaterialPageRoute(builder: (BuildContext context){
+                return JokeDetails(isInTabletLayout: false, joke: jokeSelected,);
+              }));
+    },
+    );
+  }
+  Widget _buildTabletLayou() {
+    return Row(
+      children: <Widget>[
+        Flexible (
+          flex: 1,
+          child: Material(
+            elevation: 13,
+            child: JokeListing(
+                jokeSelectedCallback: (joke) {
+                  setState(() {
+                    _selectedJode = joke;
+                  });
+                }, jokeSelected: _selectedJode,),
+          ),
+        ),
+        Flexible(
+          flex: 3,
+          child: JokeDetails(isInTabletLayout: true, joke: _selectedJode),
+        )
+      ],
+    );
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Widget content;
+    var shortestSide = MediaQuery.of(context).size.shortestSide;
+    var orientation = MediaQuery.of(context).orientation;
+
+    if (orientation == Orientation.portrait && shortestSide < tabletBreakpoint) {
+      content = _buildMobileLayout();
+
+    }else {
+      content = _buildTabletLayou();
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Jokes"),
+      ),
+      body: content,
+    );
   }
 }
